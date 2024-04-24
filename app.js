f4b7df7d71e8e2e5c3f957da6fd9c4e795273255
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-23 22:16:19 -0400

    addItem() function

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElements;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if (value !== "" && editFlag === false) {
+  } else if (value !== "" && editFlag === true) {
+  }
+}
 
 // ****** LOCAL STORAGE **********
 
