8f19f763778033bbd51e51cbf05d306b32fe12cf
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-23 23:18:20 -0400

    add item to list functionality

--- app.js
+++ app.js
@@ -21,11 +21,53 @@ function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value !== "" && editFlag === false) {
-  } else if (value !== "" && editFlag === true) {
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    //  add class
+    element.classList.add("grocery-item");
+    //add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    // append child
+    list.appendChild(element);
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
+  } else if (value && editFlag) {
+  } else {
+    displayAlert("empty value", "danger");
   }
 }
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1200);
+}
+
+// set back to default
+function setBackToDefault() {}
 
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {}
 
 // ****** SETUP ITEMS **********
